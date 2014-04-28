50a266058d74110416dc3b2e1a5f31e710f414d8
Dan Seminara <semi225599@gmail.com>
2014-04-28 15:50:36 -0400

    Initial code outline

---
+++ src/basegame/basegame.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+#Client running code, isolated and keep process available for stdin and stdout
+# maybe make this a class
+
+# Launch the client (AI) code and keep a process handle to send IO
+def launch():
+    pass
+
+# Send output from game to client
+def game_to_client():
+    pass
+
+# Send output from client to game
+def client_to_game():
+    pass
+
+# End processes if they haven't ended already and output results given from game
+def complete():
+    pass
+
+if __name__ == '__main__':
+    pass
