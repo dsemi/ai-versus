e76b9e67a87be6dd9a4b8fd0de93c939028c6b08
Dan Seminara <semi225599@gmail.com>
2014-04-28 23:34:07 -0400

    Fleshed out more details, using subprocess and Queue for data relay

--- src/basegame/basegame.py
+++ src/basegame/basegame.py
@@ -1,22 +1,46 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-#Client running code, isolated and keep process available for stdin and stdout
-# maybe make this a class
+# Client running code, isolated and keep process available for stdin and stdout
+#  maybe make this a class
 
-# Launch the client (AI) code and keep a process handle to send IO
-def launch():
+from queue import Queue
+from subprocess import Popen,PIPE
+
+class BaseGame:
+    def __init__(self, players):
+        self.players = players
+        # Use list of workers
+        self.player_handles = []
+        self.initialize_game()
+
+    # Depends on the game, but all will call launch on subprocesses
+    def initialize_game():
+        for player in self.players:
+            self.player_handles.append(launch(player))
+        
+    # Launch the client (AI) code and keep a process handle to send IO
+    def launch(self, player):
+        pass
+        # will return process handle
+
+    # Send output from game to stdin client
+    def send_to_player(self, player):
+        pass
+
+    # make synchronous or asynchronous
+    # Send output from client to game
+    def receive_from_player(self, player):
+        pass
+
+    # End processes if they haven't ended already and output results given from game
+    def complete(self):
+        pass
+
+# this will read all IO and add to queues
+def monitor(worker):
     pass
-
-# Send output from game to client
-def game_to_client():
-    pass
-
-# Send output from client to game
-def client_to_game():
-    pass
-
-# End processes if they haven't ended already and output results given from game
-def complete():
+        
+class Worker:
     pass
 
 if __name__ == '__main__':
